c49a71098be204983398eab15e95c5353838eb3f
Samouczek <795kasia@gmail.com>
2021-08-25 18:08:12 +0200

    Add navigation component

--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -1,4 +1,154 @@
-import React from "react";
-export default function Navigation (){
-    return <></>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    headerNavigation: {
+        backgroundColor: '#00bcd4',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('xs')]: {
+            display: 'block',
+        },
+    },
+
+    inputRoot: {
+        color: 'inherit',
+    },
+
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+}));
+
+export default function Navigation() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <Button color="inherit">Profil</Button>
+            </MenuItem>
+            <MenuItem>
+                <Button color="inherit">Kalorie</Button>
+            </MenuItem>
+            <MenuItem>
+                <Button color="inherit">Pomiar ciała</Button>
+            </MenuItem>
+            <MenuItem>
+                <Button color="inherit">Produkty</Button>
+            </MenuItem>
+            <MenuItem>
+                <Button color="inherit">Jadłospis</Button>
+            </MenuItem>
+
+            <MenuItem>
+                <IconButton
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>ZALOGUJ</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static" className={classes.headerNavigation}>
+                <Toolbar>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        Kalkulator żywności
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <Button color="inherit" className={classes.menuButton}>Profil</Button>
+                        <Button color="inherit" className={classes.menuButton}>Kalorie</Button>
+                        <Button color="inherit" className={classes.menuButton}>Pomiar ciała</Button>
+                        <Button color="inherit" className={classes.menuButton}>Produkty</Button>
+                        <Button color="inherit" className={classes.menuButton}>Jadłospis</Button>
+                        <Button color="inherit" className={classes.menuButton}>Login</Button>
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+        </div>
+    );
 }
