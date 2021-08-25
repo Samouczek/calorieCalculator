3ed375944ce4595054d0560bb237c48c5ac65961
Samouczek <795kasia@gmail.com>
2021-08-25 11:04:08 +0200

    basic structure

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "./Header";
+
+const Home = () => {
+    return (
+        <div>
+            Home Test
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
