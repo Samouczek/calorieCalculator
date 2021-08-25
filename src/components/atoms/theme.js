8613d7344bb0ea9ef0356925c26bcdaf6e3aa4df
Samouczek <795kasia@gmail.com>
2021-08-25 18:10:40 +0200

    Add thrmr component

---
+++ src/components/atoms/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#64d8cb',
+            main: '#26a69a',
+            dark: '#00766c',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffff56',
+            main: '#ffea00',
+            dark: '#c7b800',
+            contrastText: '#000',
+        },
+    },
+});
