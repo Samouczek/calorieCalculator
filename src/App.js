c2cf80895fa2e05304bc20f33e589f37a9ec58a6
Samouczek <795kasia@gmail.com>
2021-08-25 15:12:01 +0200

    Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import Home from "./components/Home"
+import {BrowserRouter, Switch, Route, Link, NavLink} from "react-router-dom";
+import Home from "./components/pages/Home"
+import NotFound from "./components/pages/NotFound";
+import Navigation from "./components/organisms/Navigation";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+          <Navigation/>
         <Switch>
           <Route exact path="/" component={Home} />
           {/*<Route exact path="/contact" component={Contact} />*/}
+            <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     </>
