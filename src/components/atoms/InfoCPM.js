8bd360fb2d690caf2a0ca67298e0a4aca73f644c
Samouczek <795kasia@gmail.com>
2021-08-26 17:10:32 +0200

    Add grid to infoCPM abd infoPPM components

--- src/components/atoms/InfoCPM.js
+++ src/components/atoms/InfoCPM.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import theme from "../atoms/theme";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
     root: {
@@ -43,16 +44,22 @@ export default function InfoCPM() {
                     Całkowita przemiana materii (CPM)
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
-                    <p style={{textAlign: 'justify'}}><strong>Całkowita przemiana materii (CPM)</strong> (<em>ang.
-                        total daily energy expenditure – TDEE</em>) –  suma wszystkich wydatków energetycznych
-                        człowieka, które ponosi podczas aktywności fizycznej w ciągu jednego dnia.</p>
-                    <p>Całkwitą przemianę materii można obliczyć posługując się uproszczonym wzorem.</p>
-                    <p className={classes.formula}>CPM = PPM × PAL</p>
-                    <p className={classes.componentsFormula}> <strong color={theme.palette.primary.dark}>PPM</strong>
-                        – podstawowa przemiana materii, którą możesz <br/>
-                        obliczyć ze wzoru Harrisa i Benedicta<br/>
-                        <strong color={theme.palette.primary.dark}>PAL</strong> – współczynnik aktywności fizycznej, który możesz <br/>
-                        oszacować na podstawie ankiety</p>
+                    <Grid container spacing={2}>
+                        <Grid item sm={12} md={6}>
+                            <p style={{textAlign: 'justify'}}><strong>Całkowita przemiana materii (CPM)</strong> (<em>ang.
+                                total daily energy expenditure – TDEE</em>) –  suma wszystkich wydatków energetycznych
+                                człowieka, które ponosi podczas aktywności fizycznej w ciągu jednego dnia.</p>
+                            <p>Całkwitą przemianę materii można obliczyć posługując się uproszczonym wzorem.</p>
+                        </Grid>
+                        <Grid item sm={12} md={6}>
+                            <p className={classes.formula}>CPM = PPM × PAL</p>
+                            <p className={classes.componentsFormula}> <strong color={theme.palette.primary.dark}>PPM</strong>
+                                – podstawowa przemiana materii, którą możesz <br/>
+                                obliczyć ze wzoru Harrisa i Benedicta<br/>
+                                <strong color={theme.palette.primary.dark}>PAL</strong> – współczynnik aktywności fizycznej, który możesz <br/>
+                                oszacować na podstawie ankiety</p>
+                            </Grid>
+                    </Grid>
                 </Typography>
 
             </CardContent>
--- src/components/atoms/InfoPPM.js
+++ src/components/atoms/InfoPPM.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import theme from "../atoms/theme";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
     root: {
@@ -43,19 +44,26 @@ export default function InfoPPM() {
                     Pdstawowa przemiana materii (PPM)
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
-                    <p style={{textAlign: 'justify'}}><strong>Podstawowa przemiana materii (PPM)</strong> (<em>ang. Basal metabolic rate - BMR</em>) – najmniejsze
-                        tempo przemiany materii, zachodzącej w organizmie człowieka, niezbędne do podtrzymania
-                        podstawowych funkcji życiowych, znajdującego się w stanie czuwania, w warunkach zupełnego
-                        spokoju fizycznego i psychicznego, komfortu cieplnego, który na 12 godzin przed badaniem
-                        nie spożywał żadnych posiłków, po 3 dniach diety bezbiałkowej i po co najmniej 8 godzinach snu.</p>
-                    <p>Podstawową przemianę materii można obliczyć m. in. wg <strong>wzoru Harrisa-Benedicta</strong>.</p>
-                    <p className={classes.textFormula}>Dla kobiet wzór przyjmuje postać: </p>
-                    <p className={classes.formula}>655,1 + (9,563 × M) + (1,85 × W) − (4,676 × L)</p>
-                    <p className={classes.textFormula}>Dla mężczyzn natomiast:</p>
-                    <p className={classes.formula}>66,47 + (13,75 × M) + (5,0 × W) − (6,76 × L)</p>
-                    <p className={classes.componentsFormula}>M – masa ciała w kilogramach <br/>
-                    W – wzrost w centymetrach <br/>
-                    L – wiek w latach</p>
+                    <Grid container spacing={2}>
+                        <Grid item sm={12} md={7}>
+                                <p style={{textAlign: 'justify'}}><strong>Podstawowa przemiana materii (PPM)</strong>
+                                    (<em>ang. Basal metabolic rate - BMR</em>) – najmniejsze
+                                    tempo przemiany materii, zachodzącej w organizmie człowieka, niezbędne do podtrzymania
+                                    podstawowych funkcji życiowych, znajdującego się w stanie czuwania, w warunkach zupełnego
+                                    spokoju fizycznego i psychicznego, komfortu cieplnego, który na 12 godzin przed badaniem
+                                    nie spożywał żadnych posiłków, po 3 dniach diety bezbiałkowej i po co najmniej 8 godzinach snu.</p>
+                                <p>Podstawową przemianę materii można obliczyć m. in. wg <strong>wzoru Harrisa-Benedicta</strong>.</p>
+                        </Grid>
+                        <Grid item sm={12} md={5}>
+                            <p className={classes.textFormula}>Dla kobiet wzór przyjmuje postać: </p>
+                            <p className={classes.formula}>655,1 + (9,563 × M) + (1,85 × W) − (4,676 × L)</p>
+                            <p className={classes.textFormula}>Dla mężczyzn natomiast:</p>
+                            <p className={classes.formula}>66,47 + (13,75 × M) + (5,0 × W) − (6,76 × L)</p>
+                            <p className={classes.componentsFormula}>M – masa ciała w kilogramach <br/>
+                            W – wzrost w centymetrach <br/>
+                            L – wiek w latach</p>
+                        </Grid>
+                    </Grid>
                 </Typography>
 
             </CardContent>
