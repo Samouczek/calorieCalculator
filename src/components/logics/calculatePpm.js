4ce6bb832deabb722bd03d9adbc873d5947340cb
Samouczek <795kasia@gmail.com>
2021-08-27 10:19:03 +0200

    Small fix - calories shown without decimals

--- src/components/logics/calculatePpm.js
+++ src/components/logics/calculatePpm.js
@@ -1,8 +1,8 @@
 
 export default function calculatePpm(sex, bodyWeight, bodyGrowth, bodyAge){
         if (sex === 0){
-            return (655.1 + (9.563*bodyWeight)+(1.85*bodyGrowth)-(4.676*bodyAge)).toFixed(2);
+            return (655.1 + (9.563*bodyWeight)+(1.85*bodyGrowth)-(4.676*bodyAge)).toFixed(0);
         } else {
-            return (66.47 + (13.75 * bodyWeight) + (5.0 * bodyGrowth) - (6.76 * bodyAge)).toFixed(2);
+            return (66.47 + (13.75 * bodyWeight) + (5.0 * bodyGrowth) - (6.76 * bodyAge)).toFixed(0);
         }
 }
