da61eedb307a7e4baf065e8750a9fadf575b1d5a
Samouczek <795kasia@gmail.com>
2021-08-27 16:57:59 +0200

    improved readability in DisplayMacronutrientsInCalcCpm

--- src/components/atoms/DisplayMacronutrientsInCalcCpm.js
+++ src/components/atoms/DisplayMacronutrientsInCalcCpm.js
@@ -8,18 +8,32 @@ function DisplayMacronutrientsInCalcCpm({cpmResult, bodyWeight, palValue}){
     const macronutrients = calculateProteinCarboFat(cpmResult, bodyWeight, palValue);
 
     return <Typography >
-        białko - {macronutrients[0][0]}g - {macronutrients[0][1]} kcal - {macronutrients[0][2]}% wartości energetycznej diety<br/>
-        <em className={classes.comment}>
+        białko - { macronutrients[0][0] }g - { macronutrients[0][1] } kcal - { macronutrients[0][2] }%
+        wartości energetycznej diety<br/>
+        <em
+            className={classes.comment}
+        >
             (od 0.8g do 2g na 1kg masy ciała, 1g to 4 kcal)
-        </em> <br/>
-        węglowodany: {macronutrients[1][0]}g - {macronutrients[1][1]} kcal - {macronutrients[1][2]}% wartości energetycznej diety<br/>
-        <em className={classes.comment}>
+        </em>
+        <br/>
+        węglowodany: { macronutrients[1][0] }g - { macronutrients[1][1] } kcal - { macronutrients[1][2] }%
+        wartości energetycznej diety
+        <br/>
+        <em
+            className={classes.comment}
+        >
             (od 45% do 65% wartości energetycznej, 1g to 4 kcal)
-        </em> <br/>
-        tłuszcze: {macronutrients[2][0]}g - {macronutrients[2][1]} kcal - {macronutrients[2][2]}% wartości energetycznej diety<br/>
-        <em className={classes.comment}>
+        </em>
+        <br/>
+        tłuszcze: { macronutrients[2][0] }g - { macronutrients[2][1] } kcal - { macronutrients[2][2] }%
+        wartości energetycznej diety
+        <br/>
+        <em
+            className={classes.comment}
+        >
             (od 20% do 35% wartości energetycznej,  1g to 9 kcal)
-        </em> <br/>
+        </em>
+        <br/>
     </Typography>
 }
 export default DisplayMacronutrientsInCalcCpm;
