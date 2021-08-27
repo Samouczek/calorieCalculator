bbc1a38afe619d2a6ff9083511865911125b5638
Samouczek <795kasia@gmail.com>
2021-08-27 17:14:37 +0200

    improved readability in NotFound and Profile

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,4 +1,7 @@
 import React from "react";
+
 export  default function NotFound () {
-    return <h1>404, Nothing in here</h1>
+    return <h1>
+                404, Nothing in here
+            </h1>
 }
--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,14 +1,14 @@
+import {Container} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import React, {useState} from "react";
 import PPMForm from "../organisms/PPMForm";
 import PALClassification from "../organisms/PALClassification";
 import FreeTimeActivity from "../organisms/FreeTimeActivity";
 import WorkTimeActivity from "../organisms/WorkTimeActivity";
-import {Container} from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import InfoPPM from "../atoms/InfoPPM";
-import InfoCPM from "../atoms/InfoCPM";
 import CPMForm from "../organisms/CpmForm";
 import Copyright from "../organisms/Copyright";
+import InfoPPM from "../atoms/InfoPPM";
+import InfoCPM from "../atoms/InfoCPM";
 
 export default function Profile(){
     const [ppmValue, setPpmValue] = useState(false);
@@ -20,7 +20,7 @@ export default function Profile(){
 
     return  <Container component="main" maxWidth="lg">
         <InfoPPM/>
-        <PPMForm getParameters={handleParameters}/>
+        <PPMForm getParameters = {handleParameters}/>
         <InfoCPM/>
             <Grid container spacing={3}>
                 <Grid item sm={12} md={6} lg={4}>
@@ -33,7 +33,7 @@ export default function Profile(){
                     <PALClassification/>
                 </Grid>
             </Grid>
-        <CPMForm bodyWeight={bodyWeight} ppm={ppmValue}/>
+        <CPMForm bodyWeight = { bodyWeight } ppm = { ppmValue }/>
         <Copyright/>
     </Container>
 
