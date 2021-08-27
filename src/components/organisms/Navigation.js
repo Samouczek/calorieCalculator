74b3b2de88c9ffb5522e659b7ee9bc21c2705f2b
Samouczek <795kasia@gmail.com>
2021-08-27 17:45:23 +0200

    improved readability in Navigation and create StyleNavigation

--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -8,43 +7,13 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
-import theme from "../../styles/theme";
 import { ThemeProvider } from '@material-ui/core/styles';
+import theme from "../../styles/theme";
+import StyleNavigation from "../../styles/StyleNavigation";
 
-const useStyles = makeStyles((theme) => ({
-    grow: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-
-    },
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('xs')]: {
-            display: 'block',
-        },
-    },
-
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
-    },
-    sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
-    },
-}));
-
-export default function Navigation({username}) {
-    const classes = useStyles();
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-
+export default function Navigation( { username } ) {
+    const classes = StyleNavigation();
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
     const handleMobileMenuClose = () => {
@@ -55,67 +24,96 @@ export default function Navigation({username}) {
         setMobileMoreAnchorEl(event.currentTarget);
     };
 
-    const menuId = 'primary-search-account-menu';
-
-
     const mobileMenuId = 'primary-search-account-menu-mobile';
     const renderMobileMenu = (
         <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
+            anchorEl = {mobileMoreAnchorEl}
+            anchorOrigin = {{ vertical: 'top', horizontal: 'right' }}
+            id = {mobileMenuId}
             keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}
+            transformOrigin = {{ vertical: 'top', horizontal: 'right' }}
+            open = {isMobileMenuOpen}
+            onClose = {handleMobileMenuClose}
         >
             <MenuItem>
-                <Button color="inherit">Profil</Button>
+                <Button color="inherit">
+                    Profil
+                </Button>
             </MenuItem>
             <MenuItem>
-                <Button color="inherit">Kalorie</Button>
+                <Button color="inherit">
+                    Kalorie
+                </Button>
             </MenuItem>
             <MenuItem>
-                <Button color="inherit">Pomiar ciała</Button>
+                <Button color="inherit">
+                    Pomiar ciała
+                </Button>
             </MenuItem>
             <MenuItem>
-                <Button color="inherit">Produkty</Button>
+                <Button color="inherit">
+                    Produkty
+                </Button>
             </MenuItem>
             <MenuItem>
-                <Button color="inherit">Jadłospis</Button>
+                <Button color="inherit">
+                    Jadłospis
+                </Button>
             </MenuItem>
-
             <MenuItem>
-                <Button color={'inherit'}
-                        className={classes.menuButton}
+                <Button color = {'inherit'}
+                        className = { classes.menuButton }
                 >
-                    <i className="material-icons"
+                    <i className = "material-icons"
                     >
                         account_circle
                     </i>
-                    {username ? username: 'Zaloguj'}
+                    { username ? username: 'Zaloguj' }
                 </Button>
             </MenuItem>
         </Menu>
     );
 
     return (
-        <ThemeProvider theme={theme}>
-        <div className={classes.grow}>
-            <AppBar position="static">
+        <ThemeProvider theme = { theme }>
+        <div className = { classes.grow }>
+            <AppBar position = "static">
                 <Toolbar>
-                    <Typography className={classes.title} variant="h6" noWrap>
+                    <Typography className = { classes.title }
+                                variant = "h6"
+                                noWrap
+                    >
                         Kalkulator żywności
                     </Typography>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                        <Button color="inherit" className={classes.menuButton}>Profil</Button>
-                        <Button color="inherit" className={classes.menuButton}>Kalorie</Button>
-                        <Button color="inherit" className={classes.menuButton}>Pomiar ciała</Button>
-                        <Button color="inherit" className={classes.menuButton}>Produkty</Button>
-                        <Button color="inherit" className={classes.menuButton}>Jadłospis</Button>
-                        <Button color={'inherit'}
-                                className={classes.menuButton}
+                    <div className = { classes.grow }/>
+                    <div className = { classes.sectionDesktop }>
+                        <Button color = "inherit"
+                                className = { classes.menuButton }
+                        >
+                            Profil
+                        </Button>
+                        <Button color = "inherit"
+                                className = { classes.menuButton }
+                        >
+                            Kalorie
+                        </Button>
+                        <Button color = "inherit"
+                                className = { classes.menuButton }
+                        >
+                            Pomiar ciała
+                        </Button>
+                        <Button color = "inherit"
+                                className = { classes.menuButton }
+                        >
+                            Produkty
+                        </Button>
+                        <Button color = "inherit"
+                                className = { classes.menuButton }
+                        >
+                            Jadłospis
+                        </Button>
+                        <Button color = {'inherit'}
+                                className = { classes.menuButton }
                         >
                             <i className="material-icons profile_icon"
                             >
@@ -125,7 +123,7 @@ export default function Navigation({username}) {
                         </Button>
 
                     </div>
-                    <div className={classes.sectionMobile}>
+                    <div className = { classes.sectionMobile }>
                         <IconButton
                             aria-label="show more"
                             aria-controls={mobileMenuId}
@@ -138,7 +136,7 @@ export default function Navigation({username}) {
                     </div>
                 </Toolbar>
             </AppBar>
-            {renderMobileMenu}
+            { renderMobileMenu }
         </div>
         </ThemeProvider>
     );
---
+++ src/styles/StyleNavigation.js
@@ -0,0 +1,34 @@
+import {makeStyles} from "@material-ui/core/styles";
+import theme from "./theme";
+
+function StyleNavigation(){
+   return  makeStyles(() => ({
+        grow: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+
+        },
+        title: {
+            display: 'none',
+            [ theme.breakpoints.up('xs') ]: {
+                display: 'block',
+            },
+        },
+        sectionDesktop: {
+            display: 'none',
+            [ theme.breakpoints.up('md') ]: {
+                display: 'flex',
+            },
+        },
+        sectionMobile: {
+            display: 'flex',
+            [ theme.breakpoints.up('md') ]: {
+                display: 'none',
+            },
+        },
+    }));
+}
+
+export default StyleNavigation();
