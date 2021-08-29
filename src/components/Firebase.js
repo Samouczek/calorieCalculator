d8731a708f031888aef83a8684e74a9400ca275a
Samouczek <795kasia@gmail.com>
2021-08-29 10:57:29 +0200

    Add Firebase

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,6 +1,7 @@
-import app from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore,  collection, getDocs } from 'firebase/firestore/lite';
 
-const config = {
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -9,10 +10,5 @@ const config = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
-class Firebase {
-    constructor() {
-        app.initializeApp(config);
-    }
-}
-
-export default Firebase;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 import "./scss/main.scss";
-import {FirebaseContext} from "./components/Firebase";
-import Firebase from "./components/Firebase/Firebase";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-      <FirebaseContext.Provider value={new Firebase()}>
-            <App />
-      </FirebaseContext.Provider>
-  </React.StrictMode>,
+          <React.StrictMode>
+                    <App />
+          </React.StrictMode>,
   document.getElementById('root')
 );
