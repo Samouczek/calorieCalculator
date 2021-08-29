fac3aa6c8fffb4db042b44010833a5c5f79a333b
Samouczek <795kasia@gmail.com>
2021-08-29 09:44:56 +0200

    Adding Firebase

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 import "./scss/main.scss";
+import {FirebaseContext} from "./components/Firebase";
+import Firebase from "./components/Firebase/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <FirebaseContext.Provider value={new Firebase()}>
+            <App />
+      </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
